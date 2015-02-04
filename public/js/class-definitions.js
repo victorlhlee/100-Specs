394afdbce1884811fefd34a964328c926b91b1c6
Victor <viclee02@gmail.com>
2015-02-03 18:55:39 -1000

    Rainbow passes

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -165,6 +165,19 @@ var browsers = {
  *  isDouble  => true
  *
  */
+var rainbow = {
+  colors : {
+  red: "#F0280A", 
+  orange : "#FF8800", 
+  yellow : "#FFDD00", 
+  green :"#51AB0C", 
+  blue : "#1593ED", 
+  indigo : "#5215ED", 
+  violet : "#A915ED"},
+
+  isDouble : true
+
+};
 
 
 /* Step 14
