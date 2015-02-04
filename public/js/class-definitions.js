cf80eb45a7537f676ddd8eb913b4f946ab03e0fc
Victor <viclee02@gmail.com>
2015-02-03 18:24:49 -1000

    Browsers passes

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -139,6 +139,14 @@ var domains = {
  *   Konqueror  => "konqueror.org"
  *
  */
+var browsers = {
+  Chromium: "google.com",
+  Safari: "apple.com",
+  Opera: "opera.com",
+  Firefox: "mozilla.org",
+  Sleipnir: "fenrir-inc.com",
+  Konqueror: "konqueror.org",
+};
 
 
 /* Step 13
