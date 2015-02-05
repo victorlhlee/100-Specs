86b8dc0b512109501bf0da1b4ca2b119ae57cbd3
Victor <viclee02@gmail.com>
2015-02-04 22:00:55 -1000

    Vechile Class passes

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -988,10 +988,10 @@ function Animal (species, gender){
  * @param {string} model The vehicle's model
  */
 
- // function Vehicle (make, model){
- //  this.make = make;
- //  this.model = model;
- // }
+ function Vehicle (make, model){
+  this.make = make;
+  this.model = model;
+ }
 
 
 /**
