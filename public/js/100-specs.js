c2eaf8549094de0008e271905b74ee34c31af73b
Jon Borgonia <jon@gomagames.com>
2015-02-04 14:11:16 -1000

    fixes step82 vehicle.drive to be a function

--- public/js/100-specs.js
+++ public/js/100-specs.js
@@ -1111,7 +1111,7 @@ HundredSpecs.prototype = {
 	,step82: function() {
 		js.mocha.M.it("drive should be a function.",function() {
 			var myVehicle = new Vehicle('Toyota', 'FJ Cruiser');
-			js.expect.E.expect(Vehicle.drive).to.be.a("function");
+			js.expect.E.expect(myVehicle.drive).to.be.a("function");
 		});
 		js.mocha.M.it("should return 'Driving on {streetName}' if param is a string and not empty",function() {
 			var cruiser = new Vehicle('Toyota', 'FJ Cruiser');
--- src/HundredSpecs.hx
+++ src/HundredSpecs.hx
@@ -1200,7 +1200,7 @@ class HundredSpecs
   {
     M.it("drive should be a function.", function() {
       var myVehicle = untyped __js__("new Vehicle('Toyota', 'FJ Cruiser')");
-      E.expect( untyped(Vehicle.drive) ).to.be.a("function");
+      E.expect( untyped(myVehicle.drive) ).to.be.a("function");
     });
     M.it("should return 'Driving on {streetName}' if param is a string and not empty", function() {
       var cruiser = untyped __js__("new Vehicle('Toyota', 'FJ Cruiser')");
